06f36b7a1513a168055af09673a1ed7edd530a29
bennetthilberg <bennett.hilberg@gmail.com>
2023-11-05 10:53:02 -0600

    Initial commit

---
+++ compile-run.sh
@@ -0,0 +1,2 @@
+g++ -O3 -DNDEBUG -march=native -Wall -Wno-unused-function -std=c++17 -IEmpirical/include/ main.cpp -o inheritance_lab
+./inheritance_lab
---
+++ main.cpp
@@ -0,0 +1,10 @@
+
+#include "emp/base/vector.hpp"
+#include "emp/base/Ptr.hpp"
+#include <ostream>
+
+
+int main() {
+    
+
+}
